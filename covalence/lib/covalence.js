d9e2db6fef82ba0aa67382739009066ef698bfc1
Russell Fisher <russell_fisher@Russells-MacBook-Pro.local>
2016-08-17 21:00:01 -0600

    new keymaps and beginning auth

--- covalence/lib/covalence.js
+++ covalence/lib/covalence.js
@@ -11,11 +11,14 @@ var config = {
 };
 firebase.initializeApp(config);
 
+var provider = new firebase.auth.GoogleAuthProvider();
+
 import CovalenceView from './covalence-view';
 import {
     CompositeDisposable
 } from 'atom';
 
+// var enemiesCursor;
 var changedData = firebase.database().ref('projects');
 changedData.on('value', function(pulledData) {
     atom.workspace.observeTextEditors(function(editor) {
@@ -23,6 +26,7 @@ changedData.on('value', function(pulledData) {
         //     [0, 0],
         //     [0, 0]
         // ], pulledData.val().pageData);
+        // enemiesCursor = pulledData.val().bufferRow;
         editor.setText(pulledData.val().pageData);
     });
 });
@@ -45,7 +49,9 @@ export default {
 
         // Register command that toggles this view
         this.subscriptions.add(atom.commands.add('atom-workspace', {
-            'covalence:toggle': () => this.toggle()
+            'covalence:toggle': () => this.toggle(),
+            'covalence:newSession': () => this.newSession(),
+            'covalence:authentication': () => this.authenticate()
         }));
     },
 
@@ -63,9 +69,17 @@ export default {
 
     toggle() {
         var pageData;
+        var cursorPosition;
         console.log('Covalence was toggled!');
         atom.workspace.observeTextEditors(function(editor) {
-            pageData = editor.getText();
+            editor.onDidChange(function() {
+                pageData = editor.getText();
+                sendData(pageData);
+                // myCursorPosition = editor.getCursorBufferPosition();
+                // console.log(cursorPosition);
+                // editor.addCursorAtScreenPosition(myCursorPosition);
+                // editor.addCursorAtScreenPosition(enemiesCuror)
+            });
         });
 
         function sendData(pageData) {
@@ -75,7 +89,60 @@ export default {
                 "pageData": pageData
             });
         }
-        sendData(pageData);
-    }
+        // sendData(pageData);
+    },
 
+    newSession() {
+        console.log("This is the second hot key");
+    },
+
+    authenticate() {
+        console.log("This is to log in with google");
+        // return (
+        //     this.modalPanel.isVisible() ?
+        //     this.modalPanel.hide() :
+        //     this.modalPanel.show()
+        // );
+
+        function onSignIn(googleUser) {
+            console.log('Google Auth Response', googleUser);
+            // We need to register an Observer on Firebase Auth to make sure auth is initialized.
+            var unsubscribe = firebase.auth().onAuthStateChanged(function(firebaseUser) {
+                unsubscribe();
+                // Check if we are already signed-in Firebase with the correct user.
+                if (!isUserEqual(googleUser, firebaseUser)) {
+                    // Build Firebase credential with the Google ID token.
+                    var credential = firebase.auth.GoogleAuthProvider.credential(
+                        googleUser.getAuthResponse().id_token);
+                    // Sign in with credential from the Google user.
+                    firebase.auth().signInWithCredential(credential).catch(function(error) {
+                        // Handle Errors here.
+                        var errorCode = error.code;
+                        var errorMessage = error.message;
+                        // The email of the user's account used.
+                        var email = error.email;
+                        // The firebase.auth.AuthCredential type that was used.
+                        var credential = error.credential;
+                        // ...
+                    });
+                } else {
+                    console.log('User already signed-in Firebase.');
+                }
+            });
+        }
+
+        function isUserEqual(googleUser, firebaseUser) {
+            if (firebaseUser) {
+                var providerData = firebaseUser.providerData;
+                for (var i = 0; i < providerData.length; i++) {
+                    if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+                        providerData[i].uid === googleUser.getBasicProfile().getId()) {
+                        // We don't need to reauth the Firebase connection.
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
 };
